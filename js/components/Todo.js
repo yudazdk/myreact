8300b2773363cad4511f4d0dfc79da4b1b0a7e9a
ssh://yehuda@bazz6.lnx.biu.ac.il/www/apache2/git/dolev6 <yehuda.zadik@mail.biu.ac.il>
2016-10-21 19:27:16 +0300

    Arrange the return object of render

--- js/components/Todo.js
+++ js/components/Todo.js
@@ -1,15 +1,24 @@
 import React from "react"
 import { connect } from "react-redux"
 
+@connect((store) => {
+    return {
+        todos: store.todos,
+    };
+})
 
 export default class Todo extends React.Component {
     render() {
         var checked = this.props.finished ? "checked" : "";
 
+        console.log(this);
+
         return <li class="ui-state-default">
             <div class={checked}>
                 <label>{this.props.item.text}</label>
-                <span class="todo-buttons pull-right"><button class="btn">Delete</button></span>
+                <span class="todo-buttons pull-right">
+                    <button class="btn">Delete</button>
+                </span>
             </div>
         </li>
     }
