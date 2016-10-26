2d1d8afa9ea8c13f130ae00bc35f3731b680decf
ssh://yehuda@bazz6.lnx.biu.ac.il/www/apache2/git/dolev6 <yehuda.zadik@mail.biu.ac.il>
2016-10-26 22:04:02 +0300

    EditTodo get item text

--- js/components/EditTodo.js
+++ js/components/EditTodo.js
@@ -11,9 +11,22 @@ import { IndexLink, Link } from "react-router";
 
 
 export default class EditTodo extends React.Component {
+    onEditClick() {
+        //
+    }
+
     render() {
+
+        var idParam = this.props.params.id;console.log('param: ' + idParam);
+
+        var currentTodo = this.props.todos.todos.filter(function(todo) {
+            return todo.id == idParam;
+        });
+
+        var text2Edit = currentTodo[0].text;
+
         return <div class="new-todo">
-            <input type="text" id="add_todo" class="form-control add-todo" placeholder="New todo"/>
+            <input type="text" id="add_todo" class="form-control add-todo" placeholder="New todo" value={text2Edit}/>
             <span><button class="btn btn-success pull-left">Edit Todo</button></span>
         </div>
     }
